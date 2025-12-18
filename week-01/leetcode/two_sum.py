f24b557f7c1a4c241132cfa51b196b46dd9e8b66
great nnaji <nnajiuchegreat2004@gmail.com>
2025-12-18 14:02:45 -0600

    two sum done

---
+++ week-01/leetcode/two_sum.py
@@ -0,0 +1,21 @@
+def twoSum(nums: list[int], target: int) -> list[int]:
+    index_to_value_map = {}
+    return_list = []
+    for index, value in enumerate(nums):
+        index_to_value_map[value] = index
+    
+    for index, value in enumerate(nums):
+        difference = target - value
+        if difference in index_to_value_map and index_to_value_map[difference] != index:
+            return_list.append(index)
+            return_list.append(index_to_value_map[difference])
+            break
+    
+    if not return_list:
+        print("The value you are looking for is not contained in list")
+    else:
+        print(return_list)
+
+    return return_list
+
+twoSum([5,5], 10)
