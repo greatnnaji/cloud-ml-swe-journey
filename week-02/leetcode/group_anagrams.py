c59d6fc4a7c5ec2b2d1e0f5bc26d7279e918617f
great nnaji <nnajiuchegreat2004@gmail.com>
2026-01-01 19:59:50 -0600

    completed groupanagrams

---
+++ week-02/leetcode/group_anagrams.py
@@ -0,0 +1,19 @@
+def groupAnagrams(strs: list[str]) -> list[list[str]]:
+    anagrams_dict = dict()
+    for string in strs:
+        sorted_chars = sorted(string)
+        key = ''.join(sorted_chars)
+
+        if key in anagrams_dict:
+            anagrams_dict[key].append(string)
+        else:
+            anagrams_dict[key] = [string]
+        
+    result = []
+    for value in anagrams_dict.values():
+        result.append(value)
+    print(result)
+    return result
+
+strs = [""]
+groupAnagrams(strs)
