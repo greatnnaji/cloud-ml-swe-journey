eecddf3369a160175546717751eb37d9595389dd
great nnaji <nnajiuchegreat2004@gmail.com>
2026-01-13 11:23:04 -0600

    did encode and decode, currently testing edge cases

---
+++ week-03/leetcode/encode_and_decode.py
@@ -0,0 +1,13 @@
+def encode(strs: list[str]) -> str:
+    result_str = ':'.join(strs)
+    return result_str
+
+def decode(s: str) -> list[str]:
+    result_list = s.split(':')
+    return result_list
+
+dummy_input = [""]
+encoded = encode(dummy_input)
+print(f"Encoded: {encoded}")
+decoded = decode(encoded)
+print(f"Decoded: {decoded}")
