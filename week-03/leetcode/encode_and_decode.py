13580436cd5b08c3e605e6469c9ea043e6384e4a
great nnaji <nnajiuchegreat2004@gmail.com>
2026-01-13 17:13:05 -0600

    working on new solution for encode-decode, currently writing decode function

--- week-03/leetcode/encode_and_decode.py
+++ week-03/leetcode/encode_and_decode.py
@@ -1,13 +1,37 @@
+# def encode(strs: list[str]) -> str:
+#     result_str = ':;'.join(strs)
+#     print(f"Encoding {strs} to {result_str}")
+#     return result_str
+
+# def decode(s: str) -> list[str]:
+#     result_list = s.split(':;')
+#     print(f"Decoding {s} to {result_list}")
+#     return result_list
+
+# dummy_input = []
+# encoded = encode(dummy_input)
+# print(f"Encoded: {encoded}")
+# decoded = decode(encoded)
+# print(f"Decoded: {decoded}")
+#TO-DO: try to make work
+
+# 2nd Solution
 def encode(strs: list[str]) -> str:
-    result_str = ':;'.join(strs)
+    result_str = ""
+
+    for string in strs:
+        result_str += str(len(string)) + "#" + string
+
     return result_str
 
 def decode(s: str) -> list[str]:
-    result_list = s.split(':;')
-    return result_list
 
-dummy_input = [""]
+    return 
+
+
+
+dummy_input = ["Hello","World"]
 encoded = encode(dummy_input)
 print(f"Encoded: {encoded}")
-decoded = decode(encoded)
-print(f"Decoded: {decoded}")
+# decoded = decode(encoded)
+# print(f"Decoded: {decoded}")
