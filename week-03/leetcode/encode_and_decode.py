96c9f63b492bc26eba0860d7732d8144c715bc44
great nnaji <nnajiuchegreat2004@gmail.com>
2026-01-13 17:42:05 -0600

    made notes on first approach and commented out test cases for second approach

--- week-03/leetcode/encode_and_decode.py
+++ week-03/leetcode/encode_and_decode.py
@@ -4,7 +4,7 @@
 #     return result_str
 
 # def decode(s: str) -> list[str]:
-#     result_list = s.split(':;')
+#     result_list += s.split(':;')
 #     print(f"Decoding {s} to {result_list}")
 #     return result_list
 
@@ -14,6 +14,10 @@
 # decoded = decode(encoded)
 # print(f"Decoded: {decoded}")
 #TO-DO: try to make work
+# The above fails edge case when input [] is given, if logic is changed to handle that, 
+# it fails edge case where [""] is given
+# Solution would be to use length of string as delimiter and add split logic as opposed to
+# using .split() alone
 
 # 2nd Solution
 def encode(strs: list[str]) -> str:
@@ -40,10 +44,8 @@ def decode(s: str) -> list[str]:
 
     return result_list
 
-
-
-dummy_input = ["HelloGreatest","World"]
-encoded = encode(dummy_input)
-print(f"Encoded: {encoded}")
-decoded = decode(encoded)
-print(f"Decoded: {decoded}")
+# dummy_input = ["HelloGreatest","World"]
+# encoded = encode(dummy_input)
+# print(f"Encoded: {encoded}")
+# decoded = decode(encoded)
+# print(f"Decoded: {decoded}")
