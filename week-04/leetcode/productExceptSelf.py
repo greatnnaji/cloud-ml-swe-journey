eef2fb7489b9706d03809d9c32f9fa7225c705ac
great nnaji <nnajiuchegreat2004@gmail.com>
2026-02-02 18:30:40 -0500

    did O(n) solution for productExceptSelf without division

--- week-04/leetcode/productExceptSelf.py
+++ week-04/leetcode/productExceptSelf.py
@@ -1,20 +1,30 @@
-# need to do in 0(n)
-# - storing results in new array would work and then divide by nums[i]
-# - possible to solve without division?
 # naive solution would be O(n^2)
+# need to do in 0(n)
+# - storing results in new array would work and then divide by nums[i] (with division)
 
-import math
+
+import numpy as np
 
 def productExceptSelf(nums: list[int]) -> list[int]:
-    total_prod = math.prod(nums)
-    new_nums = [total_prod] * len(nums)
+    left = []
+    left.append(1)
+    right = []
+    right.append(1)
 
-    for i in range(len(nums)):
-        new_nums[i] = new_nums[i]/nums[i]
+    for i, value in enumerate(nums[:-1]):
+        left.append(value * left[-1])
 
-    print(new_nums)
+    for i,value in reversed(list(enumerate(nums[1:]))): # reversed to go from right to left excluding last element
+        right.insert(0, value * right[0])
     
-    return new_nums
+    arr1 = np.array(left)
+    arr2 = np.array(right)
+
+    result_arr = np.multiply(arr1,arr2)
+
+    print(result_arr)
+
+    return result_arr
 
 # example test case
 nums = [1,2,3,4]
